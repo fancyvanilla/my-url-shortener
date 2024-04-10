7747e79f0a062ee366ff00452fafd2bd4f71b560
Mariem BenYounes <maryemby21@gmail.com>
2024-04-10 11:48:46 +0100

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 dotenv.config();
 
-const Url= require('../models/urlModel');
+const Url= require('./models/urlModel');
 const app=express();
 
 
